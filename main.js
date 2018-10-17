3e5bac4a1b87c87bcd957833e4c04fac6a892548
Anton Dzherdzh <antondzherdzh@gmail.com>
2018-10-17 10:44:02 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const secret = require('./secret');
+
+const port = 3000;
+const pathName = '127.0.0.1';
+
+http.createServer((req, res) => {
+    if(req.method === 'GET') {
+        res.write(secret.password);
+        res.write(secret.login);
+    }
+
+    res.end();
+}).listen(port, pathName, () => {
+    console.log('Start listening');
+});
